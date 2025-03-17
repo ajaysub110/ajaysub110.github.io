789995be377c3c5eb3f02015f6ae56ed30af5035
Ajay Subramanian <ajaysubramanian110@gmail.com>
2025-03-17 18:59:54 -0400

    made writings visible

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const siteConfig = {
     // Feature flags
     features: {
         // Set to true to show the Writings section, false to hide it
-        showWritings: false
+        showWritings: true
     }
 }; 
